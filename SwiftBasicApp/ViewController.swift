c211da7556e6770e0acd302a95dafef998db23fc
iy06 <yuuki9916@gmail.com>
2021-05-15 17:52:06 +0900

    Swfit sample

--- SwiftBasicApp/ViewController.swift
+++ SwiftBasicApp/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var tapLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
 
-
+    @IBAction func tap(_ sender: Any) {
+        count += 1
+        // キャスト
+        countLabel.text = String(count)
+        
+        if count > 5 {
+            imageView.image = UIImage(named: "back2")
+        }
+        
+        if  count > 10 {
+            imageView.image = UIImage(named: "back3")
+        }
+        // switch文
+        switch count {
+            case 6:
+                tapLabel.text = "6"
+                break
+            case 10:
+                tapLabel.text = "10"
+                break
+            default:
+                tapLabel.text = "TAP"
+        }
+    }
+    
 }
-
